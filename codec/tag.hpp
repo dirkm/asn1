493dbd7a69460ffe6de9ca2c226979d6f29185ea
dirk <dirk@fritz.(none)>
2010-02-23 18:50:50 +0100

    obvious fixes

--- codec/tag.hpp
+++ codec/tag.hpp
@@ -3,67 +3,67 @@
 
 namespace asn1
 {
-  namespace codec
-  {
-    class tag
-    {
-    public:
-      typedef unsigned long tag_type;
-      static const uint8_t class_bitmask=0xC0;
-      static const uint8_t format_bitmask=0x20;
-      static const uint8_t tagid_bitmask=~(class_bitmask|format_bitmask); // 0x1F
-    private:
-      static const unsigned int shift_classformat=(sizeof(tag_type)-1)*8;
-      static const tag_type id_mask=~(((tag_type)0xFF)<<shift_classformat);
-    public:
-      tag(uint8_t class_type, uint8_t format, tag_type id):
-         v((((tag_type)(class_type|format))<<shift_classformat)|id)
+   namespace codec
+   {
+      class tag
       {
-      };
+      public:
+         typedef unsigned long tag_type;
+         static const uint8_t class_bitmask='\xC0';
+         static const uint8_t format_bitmask='\x20';
+         static const uint8_t tagid_bitmask=(uint8_t)(~(class_bitmask|format_bitmask)); // 0x1F
+      private:
+         static const unsigned int shift_classformat=(sizeof(tag_type)-1)*8;
+         static const tag_type id_mask=~(((tag_type)0xFF)<<shift_classformat);
+      public:
+         tag(uint8_t class_type, uint8_t format, tag_type id):
+            v((((tag_type)(class_type|format))<<shift_classformat)|id)
+         {
+         };
 
-      uint8_t get_class() const // shifted
-      {
-	return ((v>>shift_classformat) & class_bitmask);
-      };
+         uint8_t get_class() const // shifted
+         {
+            return ((v>>shift_classformat) & class_bitmask);
+         };
 
-      uint8_t get_format() const // shifted
-      {
-	return ((v>>shift_classformat) & format_bitmask);
-      };
+         uint8_t get_format() const // shifted
+         {
+            return ((v>>shift_classformat) & format_bitmask);
+         };
 
-      tag::tag_type get_id() const
-      {
-	return v & id_mask;
-      };
+         tag::tag_type get_id() const
+         {
+            return v & id_mask;
+         };
     
-      template<typename InIt>
-      void decode(InIt& it, InIt itend)
-      {
-	uint8_t b=*it;
-	v=((tag_type)(b&(class_bitmask|format_bitmask)))<<shift_classformat;
-	if((b&tagid_bitmask)!=tagid_bitmask)
-	  {
-	    // single byte representation
-	    v|=b&tagid_bitmask;
-	  }
-	else
-	  {
-	    // multiple byte representation
-	    tag_type tag_id=0;
-	    b=*(++it);
-	    while(b&'\x80')
-	      {
-		tag_id<<=8;
-		tag_id|=b;
-		b=*(++it);
-	      }
-	    v|=tag_id;
-	  }
+         template<typename InIt>
+         void decode(InIt& it)
+         {
+            uint8_t b=*(it++);
+            v=((tag_type)(b&(class_bitmask|format_bitmask)))<<shift_classformat;
+            if((b&tagid_bitmask)!=tagid_bitmask)
+            {
+               // single byte representation
+               v|=b&tagid_bitmask;
+            }
+            else
+            {
+               // multiple byte representation
+               tag_type tag_id=0;
+               do
+               {
+                  b=*(it++);
+                  tag_id<<=7;
+                  tag_id|=b;
+               }
+               while(b&'\x80');
+               v|=tag_id;
+            }
+         };
+      private:
+         tag_type v;
       };
-    private:
-      tag_type v;
-    };
-  }
+   }
 }
 
 #endif
