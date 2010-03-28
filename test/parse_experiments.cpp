996d7c875ad073106e0e45e05652ce6c06c767c4
dirk <dirk@fritz.(none)>
2010-03-28 20:28:10 +0200

    got to compile with parameters

--- test/parse_experiments.cpp
+++ test/parse_experiments.cpp
@@ -16,22 +16,20 @@ struct manual_parser1 : boost::spirit::qi::grammar<Iterator, typename Iterator::
 {
     manual_parser1() : manual_parser1::base_type(start)
     {
-       //start=asn1::terminal::asn1_tag(asn1::codec::tag(0,0,0));
        start=asn1::terminal::asn1_tag_token(asn1::codec::tag(0,0,0));
     }
 
    boost::spirit::qi::rule<Iterator,typename Iterator::value_type()> start;
 };
 
+using boost::spirit::qi::parse;
+
 BOOST_AUTO_TEST_CASE(manual_parser_test1)
 {
    const char single_tlv[]={'\x1F','\x24','\x04','\x01','\x02','\x03','\x04'};
-   const char* it=single_tlv;
-   const char* itend=end(single_tlv);
-   asn1::test::tlv_array_iterator tlv_it(it);
-   asn1::test::tlv_array_iterator tlv_itend(itend);
-   
-   using boost::spirit::qi::parse;
+   asn1::test::tlv_array_iterator tlv_it(single_tlv);
+   asn1::test::tlv_array_iterator tlv_itend(end(single_tlv));
+
    bool r = parse(
       tlv_it, tlv_itend,
       manual_parser1<asn1::test::tlv_array_iterator>()
@@ -52,30 +50,25 @@ struct manual_parser2 : boost::spirit::qi::grammar<Iterator, typename Iterator::
 BOOST_AUTO_TEST_CASE(manual_parser_test2)
 {
    const char single_tlv[]={'\x1F','\x24','\x04','\x01','\x02','\x03','\x04'};
-   const char* it=single_tlv;
-   const char* itend=end(single_tlv);
-   asn1::test::tlv_array_iterator tlv_it(it);
-   asn1::test::tlv_array_iterator tlv_itend(itend);
+   asn1::test::tlv_array_iterator tlv_it(single_tlv);
+   asn1::test::tlv_array_iterator tlv_itend(end(single_tlv));
    
-   using boost::spirit::qi::parse;
    bool r = parse(
       tlv_it, tlv_itend,
       manual_parser2<asn1::test::tlv_array_iterator>()
       );
 }
 
-// BOOST_AUTO_TEST_CASE(integrated_parser)
-// {
-//    const char single_tlv[]={'\x1F','\x24','\x04','\x01','\x02','\x03','\x04'};
-//    const char* it=single_tlv;
-//    const char* itend=end(single_tlv);
-//    asn1::test::tlv_array_iterator tlv_it(it);
+BOOST_AUTO_TEST_CASE(integrated_parser)
+{
+   const char single_tlv[]={'\x1F','\x24','\x04','\x01','\x02','\x03','\x04'};
+   asn1::test::tlv_array_iterator tlv_it(single_tlv);
+   asn1::test::tlv_array_iterator tlv_itend(end(single_tlv));
    
-//    using boost::spirit::qi::parse;
-//    bool r = parse(
-//       it, itend,
-//       //  Begin grammar
-//       *asn1::terminal::asn1_tag(0,0,0)
-//       );
-//    //  End grammar
-// }
+   bool r = parse(
+      tlv_it, tlv_itend,
+      //  Begin grammar
+            *asn1::terminal::asn1_tag(asn1::codec::tag(0,0,0))
+      );
+   //  End grammar
+}
